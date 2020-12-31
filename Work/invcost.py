8cd4c21a9c93a2d11409a5b1f00d946ba1e023b4
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2020-12-31 11:59:39 +0530

    handling the exceptions

--- Work/invcost.py
+++ Work/invcost.py
@@ -6,16 +6,25 @@ def inventory_cost(filename):
     with open(filename) as fh:
         rows = csv.reader(fh)
         header = next(rows)
+
         total = 0.0
         for row in rows:
             name = str(row[0])
-            quant = int(row[1])
-            price = float(row[2])
-            total += quant * price
+            try:
+                quant = int(row[1])
+                price = float(row[2])
+                total += quant * price
+            except ValueError:
+                print("Bad row", row)
+
     return total
+
+
 if len(sys.argv) == 2:
     filename = sys.argv[1]
+
 else:
     filename = "Data/inventory.csv"
+
 cost = inventory_cost(filename)
 print("Total cost is :", cost)
