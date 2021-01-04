886867f51da8289f79f0b116d5e5a7d86e2fa48a
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2021-01-04 17:21:54 +0530

    calculating the gain/loss with new prices list
    By creating the another function called read_prices returning dict of product.
    calculating the gain or loss with help of these 2 functions.

--- Work/report.py
+++ Work/report.py
@@ -4,14 +4,46 @@ import csv
 def read_inventory(filename):
     with open(filename) as fh:
         rows = csv.reader(fh)
-        header = next(rows)
-        inventory = list()
-        
+        headers = next(rows)
+        invent = list()
+
         for row in rows:
             name = str(row[0])
             quant = int(row[1])
             price = float(row[2])
             product = {"name": name, "quant": quant, "price": price}
-            inventory.append(product)
-            
-    return inventory
+            invent.append(product)
+
+        return invent
+
+
+def read_prices(filename):
+    with open(filename) as fh:
+        rows = csv.reader(fh)
+        product = dict()
+
+        for row in rows:
+            try:
+                product[row[0]] = float(row[1])
+            except IndexError:
+                continue
+
+        return product
+
+
+inventory = read_inventory("Data/inventory.csv")
+latest_prices = read_prices("Data/prices.csv")
+
+total_cost = 0.0
+present_cost = 0.0
+
+for prod in inventory:
+    total_cost += prod["quant"] * prod["price"]
+    present_cost += prod["quant"] * latest_prices[prod["name"]]
+
+print("total price is: ", total_cost)
+print("present value is: ", present_cost)
+print("total Gain is: ", present_cost - total_cost)
+
+
+
