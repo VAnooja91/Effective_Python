a79b6535776244f686e7ae5ce340a4a09fb8b79f
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2021-01-11 20:24:47 -0800

    adding extra arg of types in function for specific datatype

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None):
     with open(filename) as fh:
         rows = csv.reader(fh)
 
@@ -19,8 +19,12 @@ def parse_csv(filename, select=None):
 
             if indices:
                 row = [row[index] for index in indices]
-                
+
+            if types:
+                row = [typefunc(val) for typefunc, val in zip(types, row)]
+
             data = dict(zip(headers, row))
             records.append(data)
 
     return records
+
