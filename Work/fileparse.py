d8f588ec1d14035c7c5f50581fb6315f8baa5e2a
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2021-01-11 20:09:31 -0800

    adding extra argument to function for selected rows data

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,20 +1,26 @@
 import csv
 
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     with open(filename) as fh:
         rows = csv.reader(fh)
 
         headers = next(rows)
-        records = []
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
 
+        records = []
         for row in rows:
             if not row:
                 continue
+
+            if indices:
+                row = [row[index] for index in indices]
+                
             data = dict(zip(headers, row))
             records.append(data)
 
     return records
-
-
-
