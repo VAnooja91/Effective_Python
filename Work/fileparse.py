18bbd94fb890b75f32e5a09e9d5ff3b6e515657a
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2021-01-11 19:41:01 -0800

    Adding fileparse.py file

---
+++ Work/fileparse.py
@@ -0,0 +1,18 @@
+import csv
+
+
+def parse_csv(filename):
+    with open(filename) as fh:
+        rows = csv.reader(fh)
+
+        headers = next(rows)
+        records = []
+
+        for row in rows:
+            if not row:
+                continue
+            data = dict(zip(headers, rows))
+            records.append(data)
+
+        return records
+
