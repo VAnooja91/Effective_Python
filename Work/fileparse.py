da614efd95ef2ba5f542610e25a83068497e9052
VAnooja91 <76803743+VAnooja91@users.noreply.github.com>
2021-01-11 21:09:31 -0800

    parsing extra args to func are has_headers & delimiter
    has_headers is for checking if the file has headers or not
    delimiter is working for in the file data is separated with space or ","

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,30 +1,35 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     with open(filename) as fh:
-        rows = csv.reader(fh)
+        rows = csv.reader(fh, delimiter=delimiter)
 
-        headers = next(rows)
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
 
         records = []
         for row in rows:
             if not row:
                 continue
 
-            if indices:
+            if select:
                 row = [row[index] for index in indices]
 
             if types:
                 row = [typefunc(val) for typefunc, val in zip(types, row)]
 
-            data = dict(zip(headers, row))
+            if has_headers:
+                data = dict(zip(headers, row))
+            else:
+                data = tuple(row)
             records.append(data)
 
     return records
+
 
