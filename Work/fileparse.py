4d1a9533ee984746713d4e07614d05e66e0b4359
Anooja <76803743+VAnooja91@users.noreply.github.com>
2021-01-21 09:33:54 +0530

    changing the parse_csv method to work for any type of files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,45 +1,44 @@
 import csv
 
-# with open(filename) as fh:
-#     rows = csv.reader(fh, delimiter=delimiter)
 
-
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
-
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+    if isinstance(lines, str):
+        raise RuntimeError("lines argument must be list or a file object")
     if select and not has_headers:
         raise RuntimeError("select arguments requires column names")
-    with open(filename) as fh:
-        rows = csv.reader(fh, delimiter=delimiter)
 
-        if has_headers:
-            headers = next(rows)
-            if select:
-                indices = [headers.index(colname) for colname in select]
-                headers = select
-            else:
-                indices = []
+    rows = csv.reader(lines, delimiter=delimiter)
+    print(rows)
 
-        records = []
-        for lineno, row in enumerate(rows, start=1):
-            if not row:
+    if has_headers:
+        headers = next(rows)
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
+    records = []
+    for lineno, row in enumerate(rows, start=1):
+        if not row:
+            continue
+
+        if select:
+            row = [row[index] for index in indices]
+
+        if types:
+            try:
+                row = [typefunc(val) for typefunc, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    print("Row {}: couldn't convert {}".format(lineno, row))
+                    print("Row {}: Reason {}".format(lineno, e))
                 continue
-
-            if select:
-                row = [row[index] for index in indices]
-
-            if types:
-                try:
-                    row = [typefunc(val) for typefunc, val in zip(types, row)]
-                except ValueError as e:
-                    if not silence_errors:
-                        print("Row {}: couldn't convert {}".format(lineno, row))
-                        print("Row {}: Reason {}".format(lineno, e))
-                    continue
-            if has_headers:
-                data = dict(zip(headers, row))
-            else:
-                data = tuple(row)
-            records.append(data)
+        if has_headers:
+            data = dict(zip(headers, row))
+        else:
+            data = tuple(row)
+        records.append(data)
 
     return records
 
