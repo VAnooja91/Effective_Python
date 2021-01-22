7463714ff9f6668791c426ff6be11cb04d582f39
Anooja <76803743+VAnooja91@users.noreply.github.com>
2021-01-22 09:29:35 +0530

    Adding the "doc" string for better understanding

--- Work/product.py
+++ Work/product.py
@@ -1,14 +1,30 @@
 
 class Product:
+    '''
+    Class to represent a product consisting of name, quant and price
+    '''
     def __init__(self, name, quant, price):
+        '''
+        :param name:
+        :param quant:
+        :param price:
+        '''
         self.name = name
         self.quant = quant
         self.price = price
 
     def cost(self):
+        '''
+        returns cost of the product
+        :return: int of cost
+        '''
         return self.quant * self.price
 
     def sell(self, sold_quantity):
+        '''
+        sell few units of the product
+        :param sold_quantity:
+        '''
         self.quant = self.quant - sold_quantity
 
 
