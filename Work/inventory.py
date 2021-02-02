2c6d74de91f85f533367ada48af4055eedd44845
Anooja <76803743+VAnooja91@users.noreply.github.com>
2021-02-02 09:20:41 +0530

    adding few methods to make inventory more pythonic way not only a iterator

--- Work/inventory.py
+++ Work/inventory.py
@@ -6,8 +6,21 @@ class Inventory:
     def __iter__(self):
         return self._products.__iter__()
 
+    def __len__(self):
+        return len(self._products)
+
+    def __getitem__(self, index):
+        return self._products[index]
+
+    def __contains__(self, name):
+        return any([p.name == name for p in self._products])
+
     @property
     def total_cost(self):
+        '''
+        to calculate the total cost as an attribute
+        :return: int
+        '''
         return sum([p.cost for p in self._products])
 
     def total_quant(self):
@@ -16,3 +29,4 @@ class Inventory:
         for p in self._products:
             total_units[p.name] += p.quant
         return total_units
+
