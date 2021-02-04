b1bb496be062b7161aa7964bed5d79cec204fe10
Anooja <76803743+VAnooja91@users.noreply.github.com>
2021-02-04 09:27:35 +0530

    added ticker function to print the report in specified format

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,6 +1,7 @@
 from follow import follow
 import csv
-
+from report import read_inventory
+from tableformat  import create_formatter
 
 def conver_types(rows, types):
     for row in rows:
@@ -17,12 +18,33 @@ def select_columns(rows, indices):
         yield [row[index] for index in indices]
 
 
+def filter_names(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
+
+
 def parse_product_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows, [0, 1, 4])
     rows = conver_types(rows, [str, float, float])
     rows = make_dict(rows, ['name', 'price', 'change'])
     return rows
+
+
+def ticker(filename, logfilename, fmt):
+    inventory = read_inventory(filename)
+    rows = parse_product_data(follow(logfilename))
+    rows = filter_names(rows, inventory)
+
+    formatter = create_formatter(fmt)
+    formatter.headings(['Name', 'prices', 'Change'])
+    for row in rows:
+        name = row['name']
+        price = row['price']
+        change = row['change']
+        rowdata = [name, f'{price:0.2f}', f'{change:0.2f}']
+        formatter.row(rowdata)
 
 
 if __name__ == '__main__':
